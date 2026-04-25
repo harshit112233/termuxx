407c784792dca41e58912f03a4442896dc652dff
Harshit Sukhija <61389611+harshit112233@users.noreply.github.com>
2026-04-25 10:56:31 +0530

    Change intent action from DIAL to CALL

--- new.py
+++ new.py
@@ -5,6 +5,6 @@ time.sleep(5)
 phone_number = "+911234567890"
 subprocess.run([
     "am", "start",
-    "-a", "android.intent.action.DIAL",
+    "-a", "android.intent.action.CALL",
     "-d", f"tel:{phone_number}"
 ])
